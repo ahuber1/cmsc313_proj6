a0c2a9c316e2ef345b501c2286065c9f2347825e
ahuber1 <andrewhuber93@icloud.com>
2014-11-03 16:11:29 -0500

    Initial Commit

---
+++ proj6/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  proj6
+//
+//  Created by Andrew Huber on 11/3/2014.
+//  Copyright (c) 2014 Andrew Huber. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
