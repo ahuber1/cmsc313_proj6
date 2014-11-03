d72d0a6f60a9e52ee51e1afad9ce100e167693ce
ahuber1 <andrewhuber93@icloud.com>
2014-11-03 16:48:00 -0500

    Created circular.h

---
+++ proj6/circular.c
@@ -0,0 +1,12 @@
+#ifndef circular_h
+#define circular_h
+
+#include <stdio.h>
+#include "circular.h"
+
+//Initialize a circular buffer and return a pointer that can be used to specify that circular buffer in future function calls.
+cbuf * cbuf_init() {
+    
+}
+
+#endif
---
+++ proj6/circular.h
@@ -0,0 +1,48 @@
+// Comments
+
+#ifndef circular_h
+#define circular_h
+
+typedef struct {
+    unsigned int time;
+    double rate;
+} quote;
+
+typedef struct {
+    int next = -1; // default value of -1
+    void data;
+} cbuf_data;
+
+typedef struct {
+    cbuf_data buf[];
+    unsigned int first = 0;
+    unsigned int size = 0;
+    unsigned int num = 0;
+} cbuf;
+
+//Initialize a circular buffer and return a pointer that can be used to specify that circular buffer in future function calls.
+cbuf * cbuf_init() ;
+
+//Deallocate all dynamically allocated memory associated with the circular buffer referenced by cb_ptr. The programmer using your ADT promises to never use the value in cb_ptr again.
+void cbuf_delete(cbuf *cb_ptr) ;
+
+//Add a new currency exchange quote to the specified circular buffer. The time is in seconds since midnight. This function should remove any quotes in the circular buffer that are more than 5 minutes older than this quote. You should assume that the timestamps given to cbuf_update() are in non-decreasing order. During a cbuf_update(), the memory for the circular buffer may have to grow or shrink according to the rules described above.To assist in grading, please print out a diagnostic message with the old and new maximum sizes of your circular buffer when you either grow or shrink the buffer.
+void cbuf_update(cbuf *cb_ptr, unsigned int time, double rate) ;
+
+//Return the average exchange rate of the quotes currently stored in the buffer.
+double cbuf_average(cbuf *cb_ptr) ;
+
+//Return a pointer to the earliest quote currently stored in the buffer. (Earliest = smallest timestamp) The programmer using your ADT promises to just "look at" the quote and not alter it in any way.
+quote *cbuf_start(cbuf *cb_ptr) ;
+
+//Return a pointer to the latest quote currently stored in the buffer. (Latest = largest timestamp) The programmer using your ADT promises to just "look at" the quote and not alter it in any way.
+quote *cbuf_end(cbuf *cb_ptr) ;
+
+//Print the contents of the circular buffer to standard output. The output should be formatted nicely and also print out the current size, maximum size and the indices of the start and end of the buffer. (See sample runs linked below.)
+
+void cbuf_dump(cbuf *cb_ptr) ;
+
+//Same as cbuf_dump() but does not print out the quotes.
+void cbuf_stats(cbuf *cb_ptr) ;
+
+#endif
